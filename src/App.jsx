90982dacd21c0877b23b43a7ed2757e94f142ba3
Gagam98 <chldbsdud98@gmail.com>
2025-02-05 17:31:21 +0900

    250205 폰트 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import SearchPage from "@@/SearchPage";
+import LoginPage from "@@/loginPage/index.jsx";
+import SignupPage from "@@/SignupPage/index.jsx";
 import "@/App.css";
 
 function App() {
   return (
-    <div className="app-container">
+    <div className="">
       <Routes>
         <Route path="/" element={<SearchPage />} />
-        <Route path="/login" element={<div>Login Page Placeholder</div>} />
-        <Route path="/signup" element={<div>Signup Page Placeholder</div>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </div>
   );
