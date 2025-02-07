d27a80973385765e71ef45a944a41e210b81bc5f
Gagam98 <chldbsdud98@gmail.com>
2025-02-07 15:46:54 +0900

    폰트어썸 Battery

--- src/pages/InfoPage/Map/Battery/index.jsx
+++ src/pages/InfoPage/Map/Battery/index.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "@@/InfoPage/Map/Battery/Battery.css";
 
 const Battery = () => {
   return (
     <div className="battery-overlay">
       <div className="fa-stack fa-4x battery battery-drain">
-        <i className="fa fa-stack-1x fa-battery-0"></i>
-        <i className="fa fa-stack-1x fa-battery-1"></i>
-        <i className="fa fa-stack-1x fa-battery-2"></i>
-        <i className="fa fa-stack-1x fa-battery-3"></i>
-        <i className="fa fa-stack-1x fa-battery-4"></i>
+        <i className="fas fa-stack-1x fa-battery-empty"></i>
+        <i className="fas fa-stack-1x fa-battery-quarter"></i>
+        <i className="fas fa-stack-1x fa-battery-half"></i>
+        <i className="fas fa-stack-1x fa-battery-three-quarters"></i>
+        <i className="fas fa-stack-1x fa-battery-full"></i>
       </div>
     </div>
   );
